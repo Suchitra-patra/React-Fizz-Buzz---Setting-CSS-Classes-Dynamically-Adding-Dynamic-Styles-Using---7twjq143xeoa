a314b3fde52da54295676dce455ef6f839b37bdc
Suchitra-patra <suchitra3576@gmail.com>
2023-02-16 19:35:43 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  
+  const [count,setCount] = useState(1)
+  const [className,setClassName] = useState('normal')
+  useEffect(()=>{
+    if(count % 15 === 0){
+      setClassName('fizzbuzz')
+    }else if(count % 5 === 0){
+      setClassName('buzz')
+    }else if(count % 3 === 0){
+      setClassName('fizz')
+    }else{
+      setClassName('normal')
+    }
+  },[count])
   return (
     <div id="main">
-
+      <button id="increment" onClick={()=>{setCount(count+1)}}>Increment</button>
+      <div id="counter" className={className}>{count}</div>
+      <button id="decrement" onClick={()=>{setCount(count-1)}}>Decrement</button>
     </div>
   )
 }
